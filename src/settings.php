adcbd78ca0e85da7c806b1a2c62dfc2f54095b63
Ghazanfar Mir <GhazanfarMir@gmail.com>
2018-01-10 22:59:23 +0000

    rolled back to getenv since $_ENV is troubling with PHPUNIT

--- src/settings.php
+++ src/settings.php
@@ -18,11 +18,11 @@ return [
 
         // Database settings
         'db' => [
-            'driver' => $_ENV['DB_DRIVER'],
-            'host' => $_ENV['DB_HOST'],
-            'database' => $_ENV['DB_NAME'],
-            'username' => $_ENV['DB_USERNAME'],
-            'password' => $_ENV['DB_PASSWORD'],
+            'driver' => getenv('DB_DRIVER'),
+            'host' => getenv('DB_HOST'),
+            'database' => getenv('DB_NAME'),
+            'username' => getenv('DB_USERNAME'),
+            'password' => getenv('DB_PASSWORD'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
